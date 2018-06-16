f6e595ab20ec4f9ff40162ab970ce999ca71ef44
Roger Diller <rogerdiller@gmail.com>
2018-06-16 15:48:32 -0400

    show json data in UserList

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -3,10 +3,21 @@ import React, { Component } from 'react';
 class UserList extends Component {
     render() {
         return (
-            <div className="App">
-                Users will Appear Here
+            <div>
+                {this.state.users.map((item, index) => (
+                    <ul className='indent' key={index}>{item.name}</ul>
+                ))}
             </div>
         );
+    }
+
+    componentWillMount() {
+
+        this.setState({users : []});
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => this.setState({users : json}))
     }
 }
 

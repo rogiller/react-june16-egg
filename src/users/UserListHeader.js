ef357a65018341132201c371b09581f01d5b6b48
Roger Diller <rogerdiller@gmail.com>
2018-06-16 17:26:42 -0400

    working CSS

--- src/users/UserListHeader.js
+++ src/users/UserListHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./UserListHeader.css";
 
 const UserListHeader = (props) => {
     if (props.displayText) return (
-        <div>
+        <div className="userListHeader">
             <h3>{props.displayText}</h3>
         </div>);
-    return (<h1>NONE</h1>);
+    return (<h1 className="nothingStyle">NONE</h1>);
 };
 
 export default UserListHeader;

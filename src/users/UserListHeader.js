09c176c2d9f20218d05743f4d97ce327445b5c5c
Roger Diller <rogerdiller@gmail.com>
2018-06-16 17:21:36 -0400

    prop jazz

--- src/users/UserListHeader.js
+++ src/users/UserListHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const UserListHeader = (props) => (
-    <div>
-        <h3>{props.displayText}</h3>
-    </div>
-);
+const UserListHeader = (props) => {
+    if (props.displayText) return (
+        <div>
+            <h3>{props.displayText}</h3>
+        </div>);
+    return (<h1>NONE</h1>);
+};
 
 export default UserListHeader;

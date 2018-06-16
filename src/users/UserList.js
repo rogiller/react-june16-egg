823c7d8d6344a42d46aefb3e4ae17596d2d8fcec
Roger Diller <rogerdiller@gmail.com>
2018-06-16 15:35:13 -0400

    created UserList

---
+++ src/users/UserList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+    render() {
+        return (
+            <div className="App">
+                Users will Appear Here
+            </div>
+        );
+    }
+}
+
+export default UserList;

7a3966a799a9b7a72782b824795001a80d99710b
Roger Diller <rogerdiller@gmail.com>
2018-06-20 17:55:15 -0400

    css changes

--- src/global/App.js
+++ src/global/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Let's Reactify!</h1>
         </header>
         <UserListContainer/>
       </div>

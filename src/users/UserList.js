e17575da00b46f8ea3f1428c8cb4e9eed3bf1781
Roger Diller <rogerdiller@gmail.com>
2018-06-20 10:22:37 -0400

    better IF condition

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -3,27 +3,33 @@ import "./UserList.css";
 
 const UserList = (props) => {
 
-    let userTable = props.users.length > 0 ?
-        <table>
-        <tbody>
-        <tr className="userListHeader">
-            <td>Name</td>
-            <td>User Name</td>
-            <td>Email</td>
-            <td>Phone</td>
-            <td>Website</td>
-        </tr>
-        {props.users.map((item, index) => (
-            <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-                <td>{item.website}</td>
-            </tr>
-        ))}
-        </tbody>
-    </table> : null;
+    let userTable = null;
+
+    if(props.users.length > 0){
+        userTable =
+            <table>
+                <tbody>
+                <tr className="userListHeader">
+                    <td>Name</td>
+                    <td>User Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                    <td>Website</td>
+                </tr>
+                {props.users.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.name}</td>
+                        <td>{item.username}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                        <td>{item.website}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>;
+    } else {
+        userTable = <p>NO RESULTS</p>
+    }
 
     return (
         <div className="userList">

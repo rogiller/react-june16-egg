7b24b5d5b198d24c2c4942585bfb30b000412366
Roger Diller <rogerdiller@gmail.com>
2018-06-20 10:15:39 -0400

    jsx variable jazz

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -2,32 +2,36 @@ import React from 'react';
 import "./UserList.css";
 
 const UserList = (props) => {
+
+    let userTable = props.users.length > 0 ?
+        <table>
+        <tbody>
+        <tr className="userListHeader">
+            <td>Name</td>
+            <td>User Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Website</td>
+        </tr>
+        {props.users.map((item, index) => (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.website}</td>
+            </tr>
+        ))}
+        </tbody>
+    </table> : null;
+
     return (
         <div className="userList">
             <h3>{props.displayText}</h3>
             <input onChange={props.handleChange}/>
             <br/>
             <br/>
-            <table>
-                <tbody>
-                    <tr className="userListHeader">
-                        <td>Name</td>
-                        <td>User Name</td>
-                        <td>Email</td>
-                        <td>Phone</td>
-                        <td>Website</td>
-                    </tr>
-                    {props.users.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.name}</td>
-                            <td>{item.username}</td>
-                            <td>{item.email}</td>
-                            <td>{item.phone}</td>
-                            <td>{item.website}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {userTable}
         </div>)
 };
 

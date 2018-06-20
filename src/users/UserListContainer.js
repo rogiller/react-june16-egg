154cee9fd202e7b1535a8f4d1bec9d8e62bce42f
Roger Diller <rogerdiller@gmail.com>
2018-06-20 19:42:51 -0400

    comments

--- src/users/UserListContainer.js
+++ src/users/UserListContainer.js
@@ -7,20 +7,27 @@ class UserListContainer extends Component {
     //Method #1 for initial state
     state = {users : [], filteredUsers: [], random: 0};
 
+    //just a good ol' normal constructor like in Java/Groovy
     constructor(props){
+        //calling super is required
         super(props);
+
         //Method #2 for initial state
         //this.state = {users: []};
+
+        //yeah, still gotta deal with "this"
         this.handleUserListChange = this.handleUserListChange.bind(this);
         this.handleRandom = this.handleRandom.bind(this);
     }
 
+    //React hook called before component mounted
     componentWillMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(json => this.setState({users : json, filteredUsers: json}))
     }
 
+    //React hook for after component mounted
     componentDidMount(){
         console.log('componentDidMount');
     }
